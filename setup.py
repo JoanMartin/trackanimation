2a359505b628e66b9a369930bbb3f48b4a5d127f
JoanMarMir <joanmartinmiralles@hotmail.com>
2017-07-12 00:24:25 +0200

    Setup.py changed

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 	long_description = f.read()
 
 
