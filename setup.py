bffa3bdbd15701c6b2e3937a80e426415acc0dbd
JoanMarMir <joanmartinmiralles@hotmail.com>
2017-07-12 01:49:58 +0200

    version changed

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
 	name='trackanimation',
 
-	version='1.0.1',
+	version='1.0.2',
 
 	description='GPS Track Animation Library',
 	long_description=long_description,
